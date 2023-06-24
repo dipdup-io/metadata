7f1926883e769671b4ea8c37060b66b315d1ecd7
Artem <aopoltorzhicky@gmail.com>
2023-06-24 14:15:28 +0200

    add providers

--- internal/ipfs/node.go
+++ internal/ipfs/node.go
@@ -53,65 +53,6 @@ func NewNode(ctx context.Context, dir string, limit int64, blacklist []string, p
 func (n *Node) Start(ctx context.Context, bootstrap ...string) error {
 	log.Info().Msg("going to connect to bootstrap nodes...")
 
-	bootstrapNodes := []string{
-		"/ip4/104.248.44.204/tcp/4001/p2p/QmWaik1eJcGHq1ybTWe7sezRfqKNcDRNkeBaLnGwQJz1Cj",
-		"/ip4/167.71.55.120/tcp/4001/p2p/QmNfpLrQQZr5Ns9FAJKpyzgnDL2GgC6xBug1yUZozKFgu4",
-		"/ip4/147.75.33.191/tcp/4001/p2p/12D3KooWPySxxWQjBgX9Jp6uAHQfVmdq8HG1gVvS1fRawHNSrmqW",
-		"/ip4/147.75.80.9/tcp/4001/p2p/12D3KooWQYBPcvxFnnWzPGEx6JuBnrbF1FZq4jTahczuG2teEk1m",
-		"/ip4/147.75.80.143/tcp/4001/p2p/12D3KooWEzCun34s9qpYEnKkG6epx2Ts9oVGRGnzCvM2s2edioLA",
-		"/ip4/147.75.84.119/tcp/4001/p2p/12D3KooWQE3CWA3MJ1YhrYNP8EE3JErGbrCtpKRkFrWgi45nYAMn",
-		"/ip4/147.75.84.173/tcp/4001/p2p/12D3KooWSafoW6yrSL7waghFAaiCqGy5mdjpQx4jn4CRNqbG7eqG",
-		"/ip4/136.144.57.15/tcp/4001/p2p/12D3KooWJEfH2MB4RsUoaJPogDPRWbFTi8iehsxsqrQpiJwFNDrP",
-		"/ip4/147.75.63.131/tcp/4001/p2p/12D3KooWHpE5KiQTkqbn8KbU88ZxwJxYJFaqP4mp9Z9bhNPhym9V",
-		"/ip4/147.75.62.95/tcp/4001/p2p/12D3KooWBHvsSSKHeragACma3HUodK5FcPUpXccLu2vHooNsDf9k",
-		"/ip4/147.75.50.141/tcp/4001/p2p/12D3KooWNCmYvqPbeXmNC4rnTr7hbuVtJKDNpL1vvNz6mq9Sr2Xf",
-		"/ip4/147.28.147.193/tcp/4001/p2p/12D3KooWDRak1XzURGh9MvGR4EWaP9kcbmdoagAcGMcNxBXXLzTF",
-		"/ip4/139.178.69.93/tcp/4001/p2p/12D3KooWRi18oHN1j8McxS9RMnuibcTwxu6VCTYHyLNH2R14qhTy",
-		"/ip4/139.178.91.227/tcp/4001/p2p/12D3KooWKhPb9tSnCqBswVfC5EPE7iSTXhbF4Ywwz2MKg5UCagbr",
-		"/ip4/139.178.91.231/tcp/4001/p2p/12D3KooWAdxvJCV5KXZ6zveTJmnYGrSzAKuLUKZYkZssLk7UKv4i",
-		"/ip4/147.75.49.91/tcp/4001/p2p/12D3KooWRgXWwnZQJgdW1GHW7hJ5UvZ8MLp7HBCSWS596PypAs8M",
-		"/ip4/139.178.88.145/tcp/4001/p2p/12D3KooWPbxiW4wFYHs7MwCQNqK9YVedH7QYZXJKMFVduhwR1Lcs",
-		"/ip4/145.40.90.155/tcp/4001/p2p/12D3KooWSH5uLrYe7XSFpmnQj1NCsoiGeKSRCV7T5xijpX2Po2aT",
-		"/dns4/node1.preload.ipfs.io/tcp/443/wss/ipfs/Qmbut9Ywz9YEDrz8ySBSgWyJk41Uvm2QJPhwDJzJyGFsD6",
-		"/dns4/node0.preload.ipfs.io/tcp/443/wss/ipfs/QmZMxNdpMkewiVZLMRxaNxUeZpDUb34pWjZ1kZvsd16Zic",
-		"/dns4/production-ipfs-peer.pinata.cloud/tcp/3000/ws/p2p/Qma8ddFEQWEU8ijWvdxXm3nxU7oHsRtCykAaVz8WUYhiKn",
-		"/ip4/104.18.21.126/tcp/4001/p2p/QmQzqxhK82kAmKvARFZSkUVS6fo9sySaiogAnx5EnZ6ZmC",
-		"/ip4/141.94.193.54/tcp/4001/p2p/12D3KooWAJJJwXsB5b68cbq69KpXiKqQAgTKssg76heHkg6mo2qB",
-		"/ip4/18.232.70.69/tcp/4001/p2p/12D3KooWAYgR87jsuQMUno9MXQHGv3A7GGf4wLPVQhSG7jPNtejk",
-		"/ip4/94.130.71.31/tcp/4001/p2p/12D3KooWJFBXTQaRhU3JJo5JiS4rHusKB7KuWmz1nfRjrkViaaMQ",
-		"/ip4/148.113.152.143/tcp/4001/p2p/12D3KooWBYznKkpGnjj2JSdbbV4nSgomi4eDSDyhfbg7962HQDD1",
-		"/ip4/75.101.168.30/tcp/4001/p2p/QmZNWqSqBRVkKkCF7UUVpwmKaNoV3yCgAcySiu33EMp1SZ",
-		"/ip4/54.163.158.127/tcp/4001/p2p/QmU5SHDWb97oSnxNzWwCdoo5mbHcBhnhhbZvJraFWPsTsd",
-		"/ip4/44.212.36.159/tcp/4001/p2p/Qme7qwEin3prdL4usjnHca7Wk2iCHd6VkTqXb337tYW3b9",
-		"/ip4/54.162.86.85/tcp/4001/p2p/QmbTcHDGsFY7C8LJFJvxVDitjj9vjnv2SCtMPpCCPGUjGa",
-		"/ip4/54.209.21.103/tcp/4001/p2p/QmTfhyfLC59LNFBKgdvoMkA9R6VgAte1h2gCs8XiFzDfFS",
-		"/ip4/54.144.87.112/tcp/4001/p2p/12D3KooWNTYSAYUWwc7QwPkAajW9UtLeatSMArRfLRJfAHwsoewH",
-		"/dns4/ipfs-swarm.fxhash2.xyz/tcp/4001/p2p/12D3KooWBpazXqzm5UnDtpTFbTkUJfXRHCCydnuFp2uq6vdzKVnF",
-		"/dns4/ipfs-swarm.dev.fxhash2.xyz/tcp/4001/p2p/12D3KooWPbN11QKCmEbMz1QYjeGaoAspmxSnGMD7T6z9JPJSWvmh",
-		"/ip4/54.152.12.107/tcp/4001/p2p/12D3KooWRiTA6r3uvnB87ntQ5eRZwTeVFGBjPUBuAHhdwTMW8Hss",
-		"/ip4/3.86.210.183/tcp/4001/p2p/12D3KooWGrmsCy5FSXTovD235fAyp25SpPuzqtQvjqNDDZj4rAmb",
-		"/ip4/54.89.217.140/tcp/4001/p2p/12D3KooWConXG4mVuWtxxubA3wfmUWdCwkL54nZ7SnUgbEDnHZkX",
-		"/ip4/54.87.223.182/tcp/4001/p2p/12D3KooWHRtiyBEGddhe7wvNz5q8A6gyDReqpzN5aiyT2gktKqXd",
-		"/ip4/128.199.70.49/tcp/4001/p2p/12D3KooWQySWhisgDXXJEJTHZiewHasbsmfAMYbERdtnAS39397v",
-		"/ip4/35.171.4.239/tcp/4001/p2p/12D3KooWHJCJJrAjSnJB9Mx9JWMeBAjgdSXrV7FwkCZk61if2bR3",
-		"/ip4/45.32.130.169/tcp/4001/p2p/12D3KooWPhuXjAH2wprpAQdtUvGmB3A8Hbw18JtL99MRvasWqrjF",
-		"/ip4/54.147.190.40/tcp/4001/p2p/12D3KooWHR1v13MD6ybgj5T3Ds56MB8LGcaXRH8W9cNLJP19AnRy",
-		"/ip4/54.80.114.62/tcp/4001/p2p/12D3KooWSMc3sjPAAxdNXPg5nUa9M76WK2Vp3uf9FhfARpnmKjEH",
-		"/ip4/54.174.102.221/tcp/4001/p2p/12D3KooWQo32RF8QSanP2LUnPnuKshqZdCFuUtypexzpAiUCK3js",
-		"/ip4/54.172.254.208/tcp/4001/p2p/12D3KooWMsupg6xmmfmRht93nmLyRizrECj4gNh4FdUvpxE5eqaW",
-		"/ip4/3.84.126.176/tcp/4001/p2p/12D3KooWCMfdY2PVSJTKDujSqVrGXmeXTbCnGfgJDXr9ghTVwfyu",
-		"/ip4/44.201.127.70/tcp/4001/p2p/12D3KooWCW7UeJXZuXrtkqTgNWnHvS76XNHF6CGguDgYujtXrV2w",
-		"/ip4/3.86.199.119/tcp/4001/p2p/12D3KooWLvXioz4eHHXtt9ugJRHZe8t7ZjmyvrhttKTeRcrgHmQg",
-	}
-
-	if len(bootstrap) > 0 {
-		bootstrapNodes = append(bootstrapNodes, bootstrap...)
-	}
-
-	// if err := connectToPeers(ctx, n.api, bootstrapNodes); err != nil {
-	// 	return errors.Wrap(err, "failed connect to peers")
-	// }
-
 	connected, err := n.api.Swarm().Peers(ctx)
 	if err != nil {
 		log.Warn().Msg("can't get perrs")
@@ -205,51 +146,6 @@ func spawn(ctx context.Context, dir string, blacklist []string, providers []Prov
 	return api, node, err
 }
 
-func connectToPeers(ctx context.Context, ipfs icore.CoreAPI, peers []string) error {
-	peerInfos := make(map[peer.ID]*peer.AddrInfo)
-	for _, addrStr := range peers {
-		addr, err := ma.NewMultiaddr(addrStr)
-		if err != nil {
-			log.Err(err).Str("address", addrStr).Msg("invalid multiaddr")
-			continue
-		}
-		pii, err := peer.AddrInfoFromP2pAddr(addr)
-		if err != nil {
-			log.Err(err).Str("address", addrStr).Msg("invalid address info")
-			continue
-		}
-		pi, ok := peerInfos[pii.ID]
-		if !ok {
-			pi = &peer.AddrInfo{ID: pii.ID}
-			peerInfos[pi.ID] = pi
-		}
-		pi.Addrs = append(pi.Addrs, pii.Addrs...)
-
-	}
-
-	var wg sync.WaitGroup
-	wg.Add(len(peerInfos))
-	for _, peerInfo := range peerInfos {
-		go func(peerInfo *peer.AddrInfo) {
-			defer wg.Done()
-			log.Info().Str("peer", peerInfo.ID.String()).Msg("connecting...")
-
-			connectCtx, cancel := context.WithTimeout(ctx, time.Second*30)
-			defer cancel()
-
-			if err := ipfs.Swarm().Connect(connectCtx, *peerInfo); err != nil {
-				log.Warn().
-					Str("peer", peerInfo.ID.String()).
-					Msgf("failed to connect: %s", err.Error())
-				return
-			}
-		}(peerInfo)
-	}
-	wg.Wait()
-
-	return nil
-}
-
 func createRepository(dir string, blacklist []string, providers []Provider) (string, error) {
 	if _, err := os.Stat(dir); err != nil {
 		if os.IsNotExist(err) {
@@ -313,22 +209,6 @@ func setupPlugins(externalPluginsPath string) error {
 func (n *Node) reconnect(ctx context.Context) {
 	defer n.wg.Done()
 
-	// connected, err := n.api.Swarm().Peers(ctx)
-	// if err != nil {
-	// 	log.Err(err).Msg("receiving peers")
-	// 	return
-	// }
-
-	// peerInfo := make([]peer.AddrInfo, len(connected))
-	// for i := range connected {
-	// 	peerInfo[i] = peer.AddrInfo{
-	// 		ID: connected[i].ID(),
-	// 		Addrs: []ma.Multiaddr{
-	// 			connected[i].Address(),
-	// 		},
-	// 	}
-	// }
-
 	ticker := time.NewTicker(time.Minute * 3)
 	defer ticker.Stop()
 
@@ -343,41 +223,9 @@ func (n *Node) reconnect(ctx context.Context) {
 				continue
 			}
 
-			// var wg sync.WaitGroup
 			for _, pi := range peers {
 				log.Info().Str("peer_id", pi.ID().String()).Msg("connected to peer")
-				// var found bool
-				// for i := range peers {
-				// 	if pi.ID.String() == peers[i].ID().String() {
-				// 		found = true
-				// 		break
-				// 	}
-				// }
-
-				// if found {
-				// 	log.Info().Str("peer_id", pi.ID.String()).Msg("connected to peer")
-				// } else {
-				// 	wg.Add(1)
-				// 	go func(p peer.AddrInfo, wg *sync.WaitGroup) {
-				// 		defer wg.Done()
-
-				// 		connectCtx, cancel := context.WithTimeout(ctx, time.Second*10)
-				// 		defer cancel()
-
-				// 		if err := n.api.Swarm().Connect(connectCtx, p); err == nil {
-				// 			log.Info().Str("peer_id", p.ID.String()).Msg("reconnected to peer")
-				// 		} else {
-				// 			log.Debug().Str("peer_id", p.ID.String()).Msgf("failed to reconnect: %s", err.Error())
-				// 		}
-				// 	}(pi, &wg)
-				// }
 			}
-
-			// wg.Wait()
-			// log.Info().Msg("reconnection completed")
-
-			// <-n.dht.RefreshRoutingTable()
-			// log.Info().Msg("refresh routing table completed")
 		}
 	}
 }
